756920c3d6bc12a257f365c6e82eae7fad8eaf37
Roberto Luna <robertocgw@gmail.com>
2022-11-09 18:10:02 -0300

    fix: req15 - correção da avaliacao

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany({}, { $set: { avaliacao: NumberInt(0) } });
 
-db.produtos.updateMany({ tags: "bovino" }, { $inc: { avaliação: 5 } });
-db.produtos.updateMany({ tags: "ave" }, { $inc: { avaliação: 3 } });
+db.produtos.updateMany({ tags: "bovino" }, { $inc: { avaliacao: 5 } });
+db.produtos.updateMany({ tags: "ave" }, { $inc: { avaliacao: 3 } });
 
 db.produtos.find({}, { nome: 1, avaliação: 1, _id: 0 });
